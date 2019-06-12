7c0372d499c70a7162763d22fbffa65c1e8085c2
charoner <charoner@users.noreply.github.com>
2019-06-12 17:35:03 +0800

    charoner

---
+++ HR.Hospital/HR.Hospital.Model/PageDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Hospital.Model
+{
+    public class PageDto<T>
+    {
+        /// <summary>
+        /// 总记录数
+        /// </summary>
+        public int Total { get; set; }
+
+        /// <summary>
+        /// 分页数据
+        /// </summary>
+        public List<T> PageList { get; set; }
+    }
+}
