bde487f6a6c5faf9bb5008b0df60ab3df15b3420
hyl0305 <51561466+hyl0305@users.noreply.github.com>
2019-06-13 15:12:08 +0800

    韩玉龙

---
+++ HR.Hospital.Client/HR.Hospital.Client/Common/PageHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace HR.Hospital.Client.Common
+{
+    /// <summary>
+    /// 分页类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class PageHelper<T> where T :class
+    {
+        public PageHelper()
+        {
+            PageList = new List<T>();
+        }
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        public int PageNum { get; set; }
+
+        /// <summary>
+        /// 每页显示几条
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 数据总条数
+        /// </summary>
+        public int PageSizes { get; set; }
+
+        /// <summary>
+        /// 查询数据集合
+        /// </summary>
+        public List<T> PageList { get; set; }
+    }
+}
