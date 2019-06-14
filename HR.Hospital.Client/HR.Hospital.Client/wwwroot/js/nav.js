b6ed9746eb71d2afefb68f0af0cf6e05dbce6a18
liuyamin <331785159@qq.com>
2019-06-15 07:07:09 +0800

    刘亚民

--- HR.Hospital.Client/HR.Hospital.Client/wwwroot/js/nav.js
+++ HR.Hospital.Client/HR.Hospital.Client/wwwroot/js/nav.js
@@ -14,16 +14,6 @@ var navs = [{
     "href": "http://localhost:57753/Department/Index",
     "spread": false
 }, {
-    "title": "按手术排班",
-    "icon": "icon-text",
-    "href": "https://www.baidu.com",
-    "spread": false
-}, {
-    "title": "按人员排班",
-    "icon": "icon-text",
-    "href": "page/links/linksList.html",
-    "spread": false
-}, {
     "title": "手术间管理",
     "icon": "&#xe631;",
     "href": "http://localhost:57753/OperationRoom/IndexRoom",
@@ -53,33 +43,35 @@ var navs = [{
     "href": "http://localhost:57753/Group/Index",
     "spread": false
 }, {
-    "title": "手术室用户管理",
-    "icon": "&#xe631;",
-    "href": "page/systemParameter/systemParameter.html",
-    "spread": false
-}, {
-    "title": "临床用户管理管理",
-    "icon": "&#xe631;",
-    "href": "page/systemParameter/systemParameter.html",
-    "spread": false
-}, {
     "title": "班次管理",
     "icon": "&#xe631;",
     "href": "http://localhost:57753/Shiftssetting/Show",
     "spread": false
-}, {
-    "title": "排班规则管理",
-    "icon": "&#xe631;",
-    "href": "page/systemParameter/systemParameter.html",
-    "spread": false
-}, {
-    "title": "审批管理",
-    "icon": "&#xe631;",
-    "href": "page/systemParameter/systemParameter.html",
-    "spread": false
-}, {
-    "title": "考勤管理",
-    "icon": "&#xe631;",
-    "href": "page/systemParameter/systemParameter.html",
-    "spread": false
 }]
+
+//    , {
+//        "title": "排班规则管理",
+//        "icon": "&#xe631;",
+//        "href": "page/systemParameter/systemParameter.html",
+//        "spread": false
+//}, {
+//        "title": "审批管理",
+//        "icon": "&#xe631;",
+//        "href": "page/systemParameter/systemParameter.html",
+//        "spread": false
+//}, {
+//        "title": "考勤管理",
+//        "icon": "&#xe631;",
+//        "href": "page/systemParameter/systemParameter.html",
+//        "spread": false
+//}, {
+//        "title": "按手术排班",
+//        "icon": "icon-text",
+//        "href": "https://www.baidu.com",
+//        "spread": false
+//}, {
+//        "title": "按人员排班",
+//        "icon": "icon-text",
+//        "href": "page/links/linksList.html",
+//        "spread": false
+//}
