c2f0d05c12184de7ba9a53d51e09596725d899d0
charoner <charoner@users.noreply.github.com>
2019-06-20 17:27:10 +0800

    ZYF

--- HR.Hospital.Client/HR.Hospital.Client/Controllers/Solitaire/SolitaireController.cs
+++ HR.Hospital.Client/HR.Hospital.Client/Controllers/Solitaire/SolitaireController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HR.Hospital.Client.Common;
+using HR.Hospital.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR.Hospital.Client.Controllers.Solitaire
 {
     public class SolitaireController : Controller
     {
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
+        }
+
+        public PageHelper<SolitaireSet> PageList(int pageIndex = 1, int pageSize = 3, string shift = "")
+        {
+            var list = HttpClientApi.GetAsync<PageHelper<SolitaireSet>>(HttpHelper.Url + "Solitaire/GetPagedList?pageIndex=" + pageIndex + "&pageSize=" + pageSize + "&shift=" + shift);
+            return list;
         }
     }
 }
--- HR.Hospital.Client/HR.Hospital.Client/Controllers/Summary/SummaryController.cs
+++ HR.Hospital.Client/HR.Hospital.Client/Controllers/Summary/SummaryController.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HR.Hospital.Client.Common;
+using HR.Hospital.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR.Hospital.Client.Controllers.Summary
 {
     public class SummaryController : Controller
     {
+        /// <summary>
+        /// 分页查询
+        /// </summary>  
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        public PageHelper<AttendanceSummary> PageList(int pageIndex = 1, int pageSize = 3, string name = "")
+        {
+            var list = HttpClientApi.GetAsync<PageHelper<AttendanceSummary>>(HttpHelper.Url + "Summary/GetPagedList?pageIndex=" + pageIndex + "&pageSize=" + pageSize  + "&name=" + name);
+            return list;
+        }
     }
 }
