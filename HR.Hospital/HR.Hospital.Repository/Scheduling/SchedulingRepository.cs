540d48003802e34f2e7e7efc4fe6224f1a2d9cbc
liuyamin <331785159@qq.com>
2019-06-21 17:29:03 +0800

    刘亚民

---
+++ HR.Hospital/HR.Hospital.Repository/Scheduling/SchedulingRepository.cs
@@ -0,0 +1,32 @@
+﻿using HR.Hospital.IRepository.Scheduling;
+using HR.Hospital.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Hospital.Repository.Scheduling
+{
+    public class SchedulingRepository : ISchedulingRepository
+    {
+        //连接数据库
+        string _conn = "Server=169.254.224.180;User Id=root;Password=123456;Database=hospitaldb";
+
+        public bool Add(Model.Scheduling scheduling)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 显示排班列表
+        /// </summary>
+        /// <returns></returns>
+        public List<Model.Scheduling> GetSchedulings()
+        {
+            using (MySqlConnection con = new MySqlConnection(_conn))
+            {
+                return null;
+            }
+        }
+    }
+}
