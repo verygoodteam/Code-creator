7dcf7ea6f0aa6291f8d34591da9b29b986b001d7
hyl0305 <51561466+hyl0305@users.noreply.github.com>
2019-06-28 11:59:11 +0800

    韩玉龙

--- HR.Hospital.Client/HR.Hospital.Client/Models/Dto/PermissionDto.cs
+++ HR.Hospital.Client/HR.Hospital.Client/Models/Dto/PermissionDto.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace HR.Hospital.Client.Models.Dto
 {
-   public class PermissionDto
+    public class PermissionDto
     {
         /// <summary>
         /// 编号
@@ -14,31 +13,37 @@ namespace HR.Hospital.Client.Models.Dto
         /// <summary>
         /// 权限名称
         /// </summary>
+        [Display(Name = "权限名称")]
         public string PermissionsName { get; set; }
 
         /// <summary>
         /// 父权限名称
         /// </summary>
+        [Display(Name = "父权限名称")]
         public string FatherName { get; set; }
 
         /// <summary>
         /// 地址
         /// </summary>
+        [Display(Name = "地址")]
         public string Url { get; set; }
 
         /// <summary>
         /// 父级编号
         /// </summary>
+        [Display(Name = "父级编号")]
         public int? Pid { get; set; }
 
         /// <summary>
         /// 是否启用
         /// </summary>
-        public int? Isnable { get; set; }
+        [Display(Name = "是否启用")]
+        public int? IsEnable { get; set; }
 
         /// <summary>
         /// 创建时间
         /// </summary>
-        public DateTime? Createtime { get; set; }
+        [Display(Name = "创建时间")]
+        public DateTime? CreateTime { get; set; }
     }
 }
